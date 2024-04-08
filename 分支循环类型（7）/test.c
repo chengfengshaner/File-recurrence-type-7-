c8cf92822b33771e968d3fa147c52b528b00984c
chengfengshaner <157273073+chengfengshaner@users.noreply.github.com>
2024-04-08 15:00:52 +0800

    Add files via upload

---
+++ 分支循环类型（7）/test.c
@@ -0,0 +1,77 @@
+//打印100 - 200之间的素数
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		int j = 0;
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+			{
+				break;
+			}
+		}
+		if (i == j)
+		{
+			printf("%d ", i);
+		}
+	}
+	return 0;
+}
+
+//int main()
+//{
+//	int count = 0;
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		int j = 0;
+//		int flag = 1;
+//		for (j = 2; j < i; j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				flag = 0;
+//				break;
+//			}
+//		}
+//		if (flag == 1)
+//		{
+//			count++;
+//			printf("%d ", i);
+//		}
+//	}
+//	printf("\ncount = %d\n", count);
+//
+//	return 0;
+//}
+//
+//#include <math.h>
+//int main()
+//{
+//	int count = 0;
+//	int i = 0;
+//	for (i = 101; i <= 200; i+=2)
+//	{
+//		int j = 0;
+//		int flag = 1;
+//		for (j = 2; j <= sqrt(i); j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				flag = 0;
+//				break;
+//			}
+//		}
+//		if (flag == 1)
+//		{
+//			count++;
+//			printf("%d ", i);
+//		}
+//	}
+//	printf("\ncount = %d\n", count);
+//
+//	return 0;
+//}
